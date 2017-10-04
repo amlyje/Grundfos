1430f892c07fe0a3a6c13883395d457e2e6bd9f9
simonlund92 <theburning_hill@hotmail.com>
2017-10-04 14:24:30 +0200

    added show-more buttons

---
+++ show-more.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Fav/LasV tabs</title>
+<!--W3, Stylesheet, JS-->
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="styles.css">
+<script src="js/js.js"></script>
+
+
+</head>
+
+<body>
+<!--Show more buttons, opening tabs-->
+<!--Favourites button-->
+<div class="tab">
+<button class="tablinks" onclick="openTab(event, 'Fav')">Show more!</button>
+</div>
+<!--Last vieved button-->
+<div class="tab">
+<button class="tablinks" onclick="openTab(event, 'LastV')">Show more!</button>
+</div>
+<!--Favourite tab-->
+<div id="Fav" class="tabcontent">
+ <div class="w3-container">
+<div class="w3-grey w3-center w3-text-blue w3-top-middle">
+<h2>My Favourites</h2>
+<p id="test"></p>
+</div>
+</div>
+</div>
+<!--Last Vieved tab-->
+<div id="LastV" class="tabcontent">
+<div class="w3-container">
+<div class="w3-grey w3-center w3-text-blue w3-top-middle">
+<h2>Last Vieved</h2>
+</div>
+</div>
+</div>
+<!--Product test-->
+<button class="" onClick="">Fav</button>
+
+
+
+
+
+</body>
+</html>
